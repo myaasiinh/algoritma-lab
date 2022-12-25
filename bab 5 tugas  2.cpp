a01c6f7ea35c44f7ac5e6ab5d369f97e8162fffe
myaasiinh <myaasiinh@gmail.com>
2022-12-25 13:48:01 +0700

    bab 5 latihan dan tugas

---
+++ bab 5 tugas  2.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+	int data;
+	Node* next;
+	public:
+	Node() {};
+	void SetData(int aData) { data = aData; }; 
+	void SetNext(Node* aNext) { next = aNext; }; 
+	int Data() { return data; }; Node* Next() { return next; }
+};
+class List
+{
+	Node *head;
+	public:
+	List() { head = NULL; };
+	void Print();
+	void Append(int data);
+	void Delete(int data);
+};
+void List::Print()
+{
+	Node *tmp = head;
+	if ( tmp == NULL )
+	{
+		cout << "EMPTY" << endl;
+		return;
+	}
+	if ( tmp->Next() == NULL )
+	{
+		cout << tmp->Data();
+		cout << " --> ";
+		cout << "NULL" << endl;
+	}
+	else
+	{
+		do
+		{
+			cout << tmp->Data();
+			cout << " --> ";
+			tmp = tmp->Next();
+		}
+		while ( tmp != NULL );
+		cout << "NULL" << endl;
+	}
+}
+void List::Append(int data)
+{
+	if(data <= 500)
+	{
+		Node* newNode = new Node();
+		newNode->SetData(data);
+		newNode->SetNext(NULL);
+		Node *tmp = head;
+		if ( tmp != NULL )
+		{
+			while ( tmp->Next() != NULL )
+			{
+				tmp = tmp->Next();
+			}
+			tmp->SetNext(newNode);
+		}
+		else
+		{
+			head = newNode;
+		}
+	}
+}
+void List::Delete(int data)
+{
+	if(data <= 500)
+	{
+		Node *tmp = head;
+		if ( tmp == NULL )
+		return;
+		if ( tmp->Next() == NULL )
+		{
+			delete tmp;
+			head = NULL;
+		}
+		else
+		{
+			Node *prev;
+			do
+			{
+				if ( tmp->Data() == data)
+				break;
+				prev = tmp;
+				tmp = tmp->Next();
+			}
+			while ( tmp != NULL );
+			prev->SetNext(tmp->Next());
+		}
+	}
+}
+int main()
+{
+
+	cout <<"Nama: Muhammad Yaasiin Hidayatulloh\n";
+	cout <<"NIM: 32602200107 \n\n";	List list;
+	list.Append(100);
+	list.Print();
+	list.Append(200);
+	list.Print();
+	list.Append(300);
+	list.Print();
+	list.Append(400);
+	list.Print();
+	list.Append(500);
+	list.Print();
+	list.Delete(400);
+	list.Print();
+	list.Delete(300);
+	list.Print();
+	list.Delete(200);
+	list.Print();
+	list.Delete(500);
+	list.Print();
+	list.Delete(100);
+	list.Print();
+}
+
+
+
